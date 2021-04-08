4cad399953c45177c892315891ca382b36ee83a8
Pierre BESSON <archie.gipsie@gmail.com>
2021-04-08 14:30:18 +0200

    move left

---
+++ TestGit/Assets/Scripts/Movement.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime);
+    }
+}
